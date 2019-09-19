6b8d4a492bcb5fd974af633e2ac96aababc8fc0c
Alejandro Tarapuez <eatarapuez@vectoritcgroup.com>
2019-09-19 14:18:37 -0500

    Commit Inicial

--- workflow/script.groovy
+++ workflow/script.groovy
@@ -1,1 +1,11 @@
-log.error issue.getSummary()
+package workflows.postfunctions
+
+import com.atlassian.jira.config.util.JiraHome
+import com.atlassian.jira.component.ComponentAccessor
+
+JiraHome jiraHome = ComponentAccessor.getComponent(JiraHome.class);
+
+Properties config=(Properties)evaluate (new File(String.format("%s/scripts/JiraCluster1/config.groovy",jiraHome.getHomePath())))
+Long customfieldPuesto =Long.parseLong(config.getProperty("fields.numeroDocumento.id"))
+
+log.error customfieldPuesto
