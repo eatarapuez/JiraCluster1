571d5298db6ba3d0749a181c0c1c3ce7a611ed41
Alejandro Tarapuez <eatarapuez@vectoritcgroup.com>
2019-09-19 15:29:01 -0500

    commit

--- workflow/PostFuntion/script.groovy
+++ workflow/PostFuntion/script.groovy
@@ -6,6 +6,8 @@ import com.atlassian.jira.component.ComponentAccessor
 JiraHome jiraHome = ComponentAccessor.getComponent(JiraHome.class);
 
 Properties config=(Properties)evaluate (new File(String.format("%s/scripts/JiraCluster1/config.groovy",jiraHome.getHomePath())))
-Long customfieldPuesto =Long.parseLong(config.getProperty("fields.numeroDocumento.id"))
+Long cfNumeroDocumento =Long.parseLong(config.getProperty("fields.numeroDocumento.id"))
+def numeroDocumento=issue.getCustomFieldValue(cfNumeroDocumento)
+log.error "id cf: "+customfieldPuesto
+log.error "valor Numero: "+ numeroDocumento
 
-log.error customfieldPuesto
